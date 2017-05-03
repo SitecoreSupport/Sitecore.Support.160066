3199cd181654eea33cd45e3c2a83ab0bd92850de
Pushpraj Ruhal <pur@sitecore.net>
2017-05-03 13:42:19 +0800

    Fixes browser freeze/unstable when changing background color of already colored table cell

---
+++ src/Sitecore.Support.160066/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.160066")]
+[assembly: AssemblyProduct("Sitecore.Support.160066")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Fixes browser freeze/unstable when changing background color of already colored table cell")]
+
